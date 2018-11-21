7a73e08ba682cad89628d69afb2176187eb8440b
Ire Aderinokun <iaderinokun@gmail.com>
2018-11-21 08:59:23 +0100

    Enable CORS

--- main.js
+++ main.js
@@ -8,37 +8,41 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+const takeScreenshot = require("./modules/take-screenshot");
+const uploadScreenshot = require("./modules/upload-screenshot");
+
 app.set('port', (process.env.PORT || 3000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const takeScreenshot = require("./modules/take-screenshot");
-const uploadScreenshot = require("./modules/upload-screenshot");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-(() => {
-  app.get("/", (req, res) => {
-    res.status(200).send("Welcome to our restful API");
-  });
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to our restful API");
+});
 
-  app.post("/upload", (req, res) => {
+app.post("/upload", (req, res) => {
 
-    if (!req.body) return res.status("400").send("Need request params");
-    if (!req.body.feature) return res.status("400").send("Feature required");
-    if (!req.body.periods) return res.status("400").send("Periods required");
+  if (!req.body) return res.status("400").send("Need request params");
+  if (!req.body.feature) return res.status("400").send("Feature required");
+  if (!req.body.periods) return res.status("400").send("Periods required");
 
-    const feature = req.body.feature;
-    const periods = req.body.periods;
-    const accessibleColours = req.body.accessibleColours ? (req.body.accessibleColours == "true") : false;
+  const feature = req.body.feature;
+  const periods = req.body.periods;
+  const accessibleColours = req.body.accessibleColours ? (req.body.accessibleColours == "true") : false;
 
-    takeScreenshot(feature, periods, accessibleColours)
-      .then((screenshot) => uploadScreenshot(feature, screenshot))
-      .then((result) => res.status(200).json(result))
-      .catch((err) => {
-        console.error(err);
-        res.status("500").json(err);
-      });
-  })
-})();
+  takeScreenshot(feature, periods, accessibleColours)
+    .then((screenshot) => uploadScreenshot(feature, screenshot))
+    .then((result) => res.status(200).json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status("500").json(err);
+    });
+});
 
 const server = app.listen(app.get('port'), function () {
     console.log("app running on port.", server.address().port);
